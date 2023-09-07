290c17dcbd6c6bc6730e4cfdba477fa26e246eca
anuja patil <ahpatil@clemson.edu>
2023-09-07 11:10:27 -0400

    MOve Zeroes

---
+++ MoveZeros.py
@@ -0,0 +1,18 @@
+################################################# Two Pointer Approach ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(1) ##########
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left , right = 0, 1
+
+        while (right < len(nums)):
+
+            if nums[left] != 0:
+                left += 1
+                right += 1
+
+            elif nums[right] == 0:
+                right += 1
+
+            else:
+                nums[left], nums[right] = nums[right], nums[left]
