84235e9be1f77504d28c0f685c1a4b4c7a45faa3
anuja patil <ahpatil@clemson.edu>
2023-09-10 11:23:34 -0400

    Find The Duplicate Number

---
+++ FindTheDuplicateNumber.py
@@ -0,0 +1,21 @@
+#################################################  Two Poniter Approach ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(1) ##########
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        slow, fast = 0, 0
+
+        while True:
+
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                slow = 0
+
+                while slow != fast:
+                    slow = nums[slow]
+                    fast = nums[fast]
+
+                return fast
