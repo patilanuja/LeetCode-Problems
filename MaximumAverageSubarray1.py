38c6321f626caf3a8bb5eef12dce7e19211b24e9
anuja patil <ahpatil@clemson.edu>
2023-09-11 13:03:54 -0400

    Maximum Average Subarray 1

---
+++ MaximumAverageSubarray1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        left, max_add, curr = 0, sum(nums[:k]), sum(nums[:k])
+        for right in range(k, len(nums)):
+            curr += nums[right]
+
+            if (right - left + 1) > k:
+                curr -= nums[left]
+                left += 1
+
+            max_add = max(curr, max_add)
+
+        return max_add / k
+
