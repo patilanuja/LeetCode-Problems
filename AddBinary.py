7ecc45589bd9cff84def765270cef489bfc2a5d6
Anuja Patil <42777296+patilanuja@users.noreply.github.com>
2023-09-18 23:41:47 -0400

    Update AddBinary.py

--- AddBinary.py
+++ AddBinary.py
@@ -2,6 +2,7 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
 
     #### 1st Approach ####
+        
         output = ''
         carry = 0
 
@@ -9,6 +10,7 @@ class Solution:
         j = len(b) - 1
 
         while i >= 0 or j >= 0:
+            
             sum = carry
             if i >= 0:
                 sum += int(a[i])
@@ -20,9 +22,11 @@ class Solution:
             carry = sum // 2
         if carry == 1:
             output += str(carry)
+            
         return output[::-1]
 
      #### 2nd Approach ####
+        
         decimal_a = int(a, 2)
         decimal_b = int(b, 2)
         summation = decimal_a + decimal_b
