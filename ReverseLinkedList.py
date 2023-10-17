8908a4e7b50f5fc88eaf8c1ec80e3fafbf85629f
anuja patil <ahpatil@clemson.edu>
2023-10-17 10:03:50 -0400

    Reverse Linked List

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,27 +1,22 @@
+################################################# Two Pointer ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(1) ##########
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if head is None or head.next is None:
-            return head
+        newList = None
+        current = head
 
-        # Init pointers
-        prev = head
-        curr = head.next
+        while current:
+            next_node = current.next
+            current.next = newList
+            newList = current
+            current = next_node
 
-        while curr:
-            temp = curr.next # save next
-            curr.next = prev # reverse node
-            prev = curr # advance prev
-            curr = temp # advance curr
-
-        # prevent cycle in the ListNode
-        head.next = None
-        
-        return prev
-
+        return newList
 
