13cfdf8eec05ce1a1fbc12cfbb4b1a3cb4f19f12
anuja patil <ahpatil@clemson.edu>
2023-10-23 11:05:42 -0400

    RotateList

---
+++ RotateList.py
@@ -0,0 +1,38 @@
+
+################################################# Two Pointer Approach ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(1) ##########
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        if head == None or head.next == None:
+            return head
+
+        curr = head
+        cnt = 1
+
+        while curr.next != None:
+            cnt += 1
+            curr = curr.next
+
+        n = n % cnt
+
+        t1 = t2 = head
+
+        while( t2.next != None):
+            t2 = t2.next
+            if n < 1:
+                t1 = t1.next
+            n -= 1
+
+        t2.next = head
+        head = t1.next
+        t1.next = None
+        
+        return head
