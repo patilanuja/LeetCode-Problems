f71afc5e9c2faf0fb855b55b912560e9ac56fa33
anuja patil <ahpatil@clemson.edu>
2023-10-30 19:24:34 -0400

    Maximum Depth Of Binary Tree

--- MaximumDepthOfBinaryTree.py
+++ MaximumDepthOfBinaryTree.py
@@ -4,30 +4,71 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+################################################# Recursion ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(h) ##########
+
+
 class Solution:
-    ## Approach 1
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
+
+        if not root:
             return 0
 
-        if root.left == root.right == None:
-            return 1
-
-        left = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
-        return max(left,right) + 1
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
     
 
-    # Approach 2
+################################################# BFS (Queue) ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(m = max no of nodes) ##########
+
     
+class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        return self.depth (root)
 
-    def depth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
+        if not root: return 0
 
-        return max(self.depth(root.left),self.depth(root.right)) + 1
+        level = 0
+        q = deque([root])
+
+        while q:
+
+            for i in range(len(q)):
+                node = q.popleft()
+
+                if node.left:
+                    q.append(node.left)
+            
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
+
+        return level
+    
+################################################# DFS (stack) ##########################################################
+########## Time Complexity: O(n) ########## Space Complexity: O(d = max depth) ##########
+  
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root: return 0
+
+        stack = [[root,1]]
+        res = 1
+
+        while stack:
+
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res,depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
+
+
+        
 
 
 
