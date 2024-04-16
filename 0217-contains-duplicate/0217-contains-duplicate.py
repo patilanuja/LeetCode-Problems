f2595afdf8e3820f613189c7cfc734cc624f5894
Anuja Patil <42777296+patilanuja@users.noreply.github.com>
2024-04-16 11:07:36 -0400

    Time: 419 ms (57.41%), Space: 32 MB (65.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,15 @@ class Solution:
         # TC: O(n) 
         # SC: O(n)
         
-        ele_to_occ = {}
+        # ele_to_occ = {}
 
-        for n in nums:
-            if n in ele_to_occ:
-                return True
-            else:
-                ele_to_occ[n] = 1
-        return False
+        # for n in nums:
+        #     if n in ele_to_occ:
+        #         return True
+        #     else:
+        #         ele_to_occ[n] = 1
+        # return False
+
+        updated_nums = set(nums)
+        
+        return False if len(nums) == len(updated_nums) else True
