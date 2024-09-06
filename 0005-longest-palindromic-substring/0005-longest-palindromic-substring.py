38d4016b2477e795416833fdca8746c19488473d
Anuja Patil <42777296+patilanuja@users.noreply.github.com>
2024-09-05 17:03:33 -0700

    Time: 353 ms (54.43%), Space: 16.7 MB (22.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,38 +1,33 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-
-        res = ""
+        
+        longest_substring = ""
         max_len = 0
 
         def expand(left, right, s):
-            nonlocal res, max_len
 
-            while left >= 0 and right < len(s) and s[left] == s[right]:
+            nonlocal longest_substring, max_len
 
-                # calculate current length of substring
+            while left >= 0  and right < len(s) and s[left] == s[right]:
+
                 current_len = right - left + 1
 
-                # if current_len > max_len update res and max_len
                 if current_len > max_len:
-                    res = s[left:right+1]
-                    max_len = current_len
+                     longest_substring = s[left:right+1]
+                     max_len = current_len
 
-                # expand substring 
                 left -= 1
                 right += 1
 
 
+
         for i in range(len(s)):
+            
+            # for even length 
+            left, right = i,i
+            expand(left, right,s)
 
-            # for odd len
-            left, right = i, i
-            expand(left,right,s)
-
-            # for even len
             left, right = i, i+1
             expand(left, right, s)
 
-        return res
-        
-
-        
+        return longest_substring
