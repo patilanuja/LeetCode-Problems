d325c0dfb673946358dbb76aefa73c3c53b60e17
Anuja Patil <42777296+patilanuja@users.noreply.github.com>
2024-09-05 17:48:01 -0700

    Time: 525 ms (96.61%), Space: 34.7 MB (30.1%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,40 +1,46 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
 
+         # TC = O(n) and SC = O(k)
+
         n = len(nums)
-        freq = {}
-        max_sum = 0
         sub_sum = 0
+        max_sum = 0
+        freq = {}
 
-        for ele in range(k):
-
-            if nums[ele] in freq:
-                freq[nums[ele]] += 1
+        for i in range(k):
+            if nums[i] in freq:
+                freq[nums[i]] += 1
             else:
-                freq[nums[ele]] = 1
+                freq[nums[i]] = 1
 
-            sub_sum += nums[ele] 
+            sub_sum += nums[i]
 
-        if len(freq) ==  k:
+        if len(freq) == k:
             max_sum = sub_sum
 
-        for ele in range(k, n):
-            sub_sum -= nums[ele-k]
+        for i in range(k,n):
+            sub_sum -= nums[i-k]
 
-            if freq[nums[ele-k]] == 1:
-                freq.pop(nums[ele-k])
+            if freq[nums[i-k]] == 1:
+                freq.pop(nums[i-k])
             else:
-                freq[nums[ele-k]] -= 1
+                freq[nums[i-k]] -= 1
 
-            sub_sum += nums[ele]
 
-            if nums[ele] in freq:
-                freq[nums[ele]] += 1
+            sub_sum += nums[i]
+
+            if nums[i] in freq:
+                freq[nums[i]] += 1
             else:
-                freq[nums[ele]] = 1
+                freq[nums[i]] = 1
 
             if len(freq) == k:
                 max_sum = max(max_sum, sub_sum)
 
         return max_sum
-            
+
+
+
+
+        
